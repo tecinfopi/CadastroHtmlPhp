c1925ce5e920c0ec3ee6f36a866a28e0cf40de4e
Nome Sobrenome <tecinfopi@yahoo.com>
2023-07-26 21:40:23 -0300

    Cadastro HTML/PHP

---
+++ Banco/Script_Tabelas.sql
@@ -0,0 +1,30 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema teste
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema teste
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `teste` DEFAULT CHARACTER SET utf8 ;
+USE `teste` ;
+
+-- -----------------------------------------------------
+-- Table `teste`.`dados`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `teste`.`dados` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `CPF` VARCHAR(14) NULL,
+  `NOME` VARCHAR(45) NULL,
+  PRIMARY KEY (`ID`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
